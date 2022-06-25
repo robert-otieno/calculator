a1e4dff7f8248e9b6ba8c2bf4d90350e836f433c
Robert Otieno <robertotieno.or@gmail.com>
2022-06-25 14:45:16 +0300

    clear all and delete functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,20 @@
 // *** Write your code here***
 
-let display = document.querySelector('input[type="text"]');
-
-let dot = null;
-// let operator = null;
-let operands = [];
-// let equals = null;
+let displayResult = document.querySelector('input[type="text"]');
+let result = '';
 
 addGlobalEventListener("click", 'input[type="button"]', (e) => {
   if (e.target.hasAttribute("data-number")) {
     appendNumber(e.target.value);
-    // console.log(`Clicked Number: ${e.target.value}`);
   } else if (e.target.hasAttribute("data-operation")) {
     chooseOperation(e.target.value);
-    // console.log(`Clicked Operator: ${typeof e.target.value}`);
   } else if (e.target.hasAttribute("data-dot")) {
-    console.log(`Clicked Dot: ${e.target.value}`);
+    appendNumber(e.target.value);
   } else if (e.target.hasAttribute("data-clear")) {
     clear();
   } else if (e.target.hasAttribute("data-delete")) {
     deleteVal();
   } else {
-    // console.log(`Clicked Equals: ${e.target.value}`);
     compute();
   }
 });
@@ -33,32 +26,30 @@ function addGlobalEventListener(type, selector, callback) {
 }
 
 // `clear()` function, which will clear all the different variables
-const clear = () => display.value = '';
-
+const clear = () => {
+  result = '';
+  updateDisplay();
+}
+// `delete()` for clearing a single number
 const deleteVal = () => {
-  // `delete()` for clearing a single number
-  display.value = display.value.slice(0, -1);
-};
-
+  result = displayResult.value.slice(0, -1)
+  updateDisplay();
+}
 // determines what will occur every time a user clicks on a number to add to the display
 const appendNumber = (number) => {
-  display.textContent = display.textContent += number;
-  display.value = display.textContent;
-
-  operands.push(parseInt(number));
-};
-
+  result = displayResult.textContent = displayResult.textContent += number;
+  updateDisplay();
+}
 // `chooseOperation(operation)` function that controls what will happen anytime a user clicks on any operation button
 const chooseOperation = (operation) => {
-  display.textContent = display.textContent += operation;
-  display.value = display.textContent;
-};
-
+  result = displayResult.textContent = displayResult.textContent += operation;
+  updateDisplay();
+}
 // takes the values inside your calculator and displays the result
 const compute = () => {
-  display.value=eval(display.value)
-};
-
+  result = eval(displayResult.value);
+  updateDisplay();
+}
 const updateDisplay = () => {
-  // lets us update the values inside of the output
+  displayResult.value = result;
 };
